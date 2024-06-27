99cea135d902ac041a16179267142a341f9bb116
Brian Svoboda <brian.e.svoboda@gmail.com>
2024-06-27 08:51:54 -0600

    Modify Coffeehouse definition

--- client/variantsIni.ts
+++ client/variantsIni.ts
@@ -451,8 +451,12 @@ connectN = 5
 promotionRegionWhite = *1 *2 *3 *4 *5 *6 *7 *8 *9
 promotionRegionBlack = *9 *8 *7 *6 *5 *4 *3 *2 *1
 
-[coffeehouse:crazyhouse]
+[coffeehouse:chess]
+startFen = rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR[] w KQkq - 0 1
+pieceDrops = true
+capturesToHand = true
 mustCapture = true
+
 
 [kamikazerooks:chess]
 extinctionValue = win
